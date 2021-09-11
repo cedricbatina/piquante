bed4f1022e536bb55ae30f39a82960c91a4e8cb1
Cedric Batina <shakagarvey@gmail.com>
2021-09-11 16:23:54 +0200

    working on the backend routes and models

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const sauceRoutes = require("./routes/sauce.route");
+const userRoutes = require("./routes/user.route");
+const path = require("path"); // getting access to the server's path
+const helmet = require("helmet"); // for security
+const app = express();
+const config = require("./config");
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB reussie !"))
+  .catch(() => console.log("Connexion à MongoDB echouee !"));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
