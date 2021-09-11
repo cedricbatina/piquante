6ba7e81a1a9c8be39ec94e52fb58ca4c1087ffba
Cedric Batina <shakagarvey@gmail.com>
2021-09-11 16:03:56 +0200

    backend added plus server created

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use(helmet());
+app.use(bodyParser.json()); // bodyParser is used by all endpoints
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
